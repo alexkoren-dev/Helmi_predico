4da32347221b4bb4c48e5f4037b2b5a22f273513
alexkoren-dev <alexkoren222@gmail.com>
2023-04-22 20:06:33 +0100

    a

--- disease_prediction/settings.py
+++ disease_prediction/settings.py
@@ -133,6 +133,9 @@ STATICFILES_DIRS = [
     
 ]
 TATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
 
 
 
