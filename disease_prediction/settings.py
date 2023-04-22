10f26c4420a15a6966d1553f1535a2e9433df3fd
alexkoren-dev <alexkoren222@gmail.com>
2023-04-22 21:47:36 +0100

    Update settings.py

--- disease_prediction/settings.py
+++ disease_prediction/settings.py
@@ -123,15 +123,11 @@ USE_L10N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-
-    os.path.join(BASE_DIR,'templates'),
-    os.path.join(BASE_DIR, 'pics'),
-    
+    os.path.join(BASE_DIR, 'templates'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
